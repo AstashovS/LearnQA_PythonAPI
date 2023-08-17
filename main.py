f1ee1e8f3e0e138452560a5654621aaa3f37eb51
Sergey <ketxavel@gmail.com>
2023-08-17 13:34:24 +0300

    Ex4: GET-запрос

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import requests
-def print_hi(name):
-    print(f'Hello from {name}')
 
 
+def get_text_from_url(url_text):
+    response = requests.get(url_text)
 
-if __name__ == '__main__':
-    print_hi('Sergey')
+    if response.status_code == 200:
+        content = response.text
+        return content
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+        return None
 
 
+url = "https://playground.learnqa.ru/api/get_text"
+response_content = get_text_from_url(url)
+
+if response_content is not None:
+    print("Response content:")
+    print(response_content)
